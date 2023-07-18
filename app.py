bf8497a1606458b860216ea7c0a0f6278820ef38
Sidnei Almeida <117240480+sid-almeida@users.noreply.github.com>
2023-07-18 20:07:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import openpyxl
 # load the data
-data = pd.read_csv('https://raw.githubusercontent.com/sid-almeida/fernando_elemar_dados/main/fernanddo.csv?token=GHSAT0AAAAAACEWO6QOTOF2TKL2W73HMRXOZFVTTXA', encoding='cp1258', index_col=0)
+data = pd.read_csv('https://raw.githubusercontent.com/sid-almeida/fernando_elemar_dados/main/fernanddo.csv', encoding='cp1258', index_col=0)
 
 with st.sidebar:
     st.image("https://github.com/sid-almeida/fernando_elemar_dados/blob/main/Brainize%20Tech%20(1).png?raw=true", width=250)
